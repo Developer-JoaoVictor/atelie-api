16f159bb242745402a18940f15bf31b93dd0b079
Developer-joaoVictor <joaoliveira.contato@hotmail.com>
2026-04-24 21:31:12 -0300

    feat(adiciona validacao com token): adiciona o jwt para fazer autenticacao via token

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,43 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+import { prisma } from '../lib/prisma'
+import { compare } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export class SessionsController {
+  async create(request: FastifyRequest, reply: FastifyReply) {
+    const sessionsSchema = z.object({
+      email: z.email(),
+      password: z.string(),
+    })
+
+    const { email, password } = sessionsSchema.parse(request.body)
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return reply.status(409).send({
+        message: 'Credenciais inválidas',
+      })
+    }
+
+    const passwordIsValid = await compare(password, user.password_hash)
+
+    if (!passwordIsValid) {
+      return reply.status(409).send({
+        message: 'Credenciais inválidas',
+      })
+    }
+
+    const token = jwt.sign({}, process.env.JWT_SECRET!, {
+      subject: user.id,
+      expiresIn: '7d',
+    })
+
+    return reply.send({ token })
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { UserController } from '../controllers/user-controller'
+import { SessionsController } from '../controllers/sessions-controller'
 
 const userController = new UserController()
+const sessionsController = new SessionsController()
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', (request, reply) => userController.create(request, reply))
+  app.post('/sessions', (request, reply) =>
+    sessionsController.create(request, reply),
+  )
 }
